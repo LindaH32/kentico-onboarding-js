f34773f09d3214f1351f8ca4ed8164f10c44a612
Linda Hanslikova <LindaH@kentico.com>
2017-05-26 20:27:19 +0200

    KA-137 - Try implement postItems and errors, is in progress

--- onboarding-task/src/actionCreators/fetchItemsFactory.ts
+++ onboarding-task/src/actionCreators/fetchItemsFactory.ts
@@ -1,10 +1,23 @@
-import { requestItems, receiveItems } from './actionCreators';
+import { requestItems, receiveItems, failToReceiveItems } from './actionCreators';
 import { IAction } from './IAction';
 
 export const fetchItemsFactory = (fetchFunction: () => Promise<Response>) => () => ((dispatch: Dispatch): Promise<IAction> => {
     dispatch(requestItems());
 
     return fetchFunction()
+      .then(response => response.json())
+      .then(json => dispatch(receiveItems(json)))
+      .catch((error: Error) => dispatch(failToReceiveItems(error)));
+  }
+);
+
+export const postItemsFactory = (postFunction: (url: string, options: any) => Promise<Response>) => (url: string, text: string) => ((dispatch: Dispatch): Promise<IAction> => {
+    dispatch(requestItems());
+
+    return postFunction(url, {
+      method: 'POST',
+      body: { text },
+    })
       .then(response => response.json())
       .then(json => dispatch(receiveItems(json)));
   }
