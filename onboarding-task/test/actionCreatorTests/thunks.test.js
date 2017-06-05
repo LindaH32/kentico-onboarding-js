ca9d6e249c6890db30efe8de8b4f79e3423eb74a
Linda Hanslikova <LindaH@kentico.com>
2017-06-05 14:14:30 +0200

    KA-137 - Try to test fetchItemsFactory, work in progress

--- onboarding-task/test/actionCreatorTests/thunks.test.js
+++ onboarding-task/test/actionCreatorTests/thunks.test.js
@@ -1,19 +1,46 @@
 import { fetchItemsFactory } from '../../src/actionCreators/fetchItemsFactory.ts';
+import { FETCH_ITEMS_REQUEST, REQUEST_ITEMS, FETCH_ITEMS_SUCCESS, FETCH_ITEMS_FAILURE } from '../../src/constants/actionTypes.ts';
 
 describe('Correctly resolves promises: ', () => {
-  it('resolves fetchItems', (done) => {
-    const promise = new Promise((resolve, reject) => {
-      resolve('Resolved');
-      reject('Rejected');
+  const item = { Id: '98dbde18-639e-49a6-8e51-603ceb2ae92d', Text: 'text'};
+  const requestAction = {
+    type: FETCH_ITEMS_REQUEST,
+    payload: {},
+  };
+
+  const receiveAction = {
+    type: FETCH_ITEMS_SUCCESS,
+    payload: { items: { 'Id': '98dbde18-639e-49a6-8e51-603ceb2ae92d', 'Text': 'text' } },
+  };
+
+  const errorAction = {
+    type: FETCH_ITEMS_FAILURE,
+    payload: { errorMessage: 'Items were not fetched' },
+  };
+
+  it('resolves fetchItems', () => {
+    return new Promise((resolve, reject) => {
+      const fetchFunction = () => () => item;
+
+      const fetchItems = fetchItemsFactory({
+        requestFunction: () => requestAction,
+        fetchFunction,
+        successFunction: () => receiveAction,
+        errorFunction: () => errorAction,
+      });
+      const tested = fetchItems();
+
+      process.nextTick(tested ? resolve(item) : reject('Items were not fetched'));
     });
 
-    // const fetchItems = fetchItemsFactory(() => promise.resolve);
+    //
+    // fetchItems.then();
     // const disp = () => undefined;
-    promise
-      .then(() => done())
-      .catch(() => {
-        fail('failed');
-        done();
-      });
+    // promise
+    //   .then(() => done())
+    //   .catch(() => {
+    //     fail('failed');
+    //     done();
+    //   });
   });
 });
