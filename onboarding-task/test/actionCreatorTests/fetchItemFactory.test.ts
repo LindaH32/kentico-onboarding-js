8067757e3787264dbcc89c9e92e04e5a27c181b2
Linda Hanslikova <LindaH@kentico.com>
2017-06-08 12:35:08 +0200

    KA-137 - Let tests for fetchItem factory work

--- onboarding-task/test/actionCreatorTests/fetchItemFactory.test.ts
+++ onboarding-task/test/actionCreatorTests/fetchItemFactory.test.ts
@@ -7,28 +7,43 @@ describe('Correctly resolves fetchItems: ', () => {
     { Id: '98dbde18-639e-49a6-8e51-603ceb2ae92d', Text: 'text' },
     { Id: '1c353e0a-5481-4c31-bd2e-47e1baf84dbe', Text: 'giraffe' },
   ];
-  // const itemJson = JSON.stringify(items);
+
   const fetchSuccess = () => Promise.resolve({ json: () => Promise.resolve(items) });
   const fetchFailImmediately = () => Promise.reject(new Error('Items could not be fetched'));
   const fetchFail = () => Promise.resolve({ json: () => Promise.reject(new Error('Items could not be fetched')) });
-  const fakeDispatch = jest.fn((action) => action);
+  let fakeDispatch: jest.Mock<Dispatch>;
   const fakeAction = (payload: string): IAction => ({ type: 'unknown', payload });
   const fakeRequest = () => fakeAction('request items');
   const fakeReceived = () => fakeAction('success');
   const fakeFailed = () => fakeAction('error');
-  const fetchItems = (fetch: () => Promise<Response2>) => fetchItemsFactory({
-    requestFunction: fakeRequest,
-    fetchFunction: fetch,
-    successFunction: fakeReceived,
-    errorFunction: fakeFailed,
+  const fetchItems = (fetch: () => Promise<ResponseWithJson>) => fetchItemsFactory({
+    fetchBegin: fakeRequest,
+    fetch: fetch,
+    success: fakeReceived,
+    error: fakeFailed,
   });
 
-  beforeEach(() => {
-    fakeDispatch.mockClear();
+  beforeEach((done) => {
+    fakeDispatch = jest.fn((action) => action);
+    done();
   });
 
-  it('dispatches requestItems', () => {
+  it('dispatches requestItems with succeeding fetch', () => {
     fetchItems(fetchSuccess)(fakeDispatch);
+    const actual = fakeDispatch.mock.calls[0];
+
+    expect(actual[0]).toEqual(fakeRequest());
+  });
+
+  it('dispatches requestItems with immediately failing fetch', () => {
+    fetchItems(fetchFailImmediately)(fakeDispatch);
+    const actual = fakeDispatch.mock.calls[0];
+
+    expect(actual[0]).toEqual(fakeRequest());
+  });
+
+  it('dispatches requestItems with failing fetch', () => {
+    fetchItems(fetchFail)(fakeDispatch);
     const actual = fakeDispatch.mock.calls[0];
 
     expect(actual[0]).toEqual(fakeRequest());
@@ -45,11 +60,13 @@ describe('Correctly resolves fetchItems: ', () => {
   });
 
   it('fails with error immediately', () => {
-    fetchItems(fetchFailImmediately)(fakeDispatch);
-    const actual = fakeDispatch.mock.calls;
+    return fetchItems(fetchFailImmediately)(fakeDispatch)
+      .then(() => {
+         const actual = fakeDispatch.mock.calls[1][0];
 
-    // expect(fakeDispatch.mock.calls.length).toBe(2);
-    expect(actual).toEqual(fakeFailed());
+         expect(actual).toEqual(fakeFailed());
+         expect(fakeDispatch.mock.calls.length).toBe(2);
+      });
   });
 
   it('fails with error', () => fetchItems(fetchFail)(fakeDispatch)
