321c410e7439d4f486ab902eaf9ca8b203b0dab2
Linda Hanslikova <LindaH@kentico.com>
2017-07-17 14:09:06 +0200

    KA-137 - Make status code check simpler

--- onboarding-task/src/actionCreators/checkStatus.ts
+++ onboarding-task/src/actionCreators/checkStatus.ts
@@ -1,5 +1,5 @@
 export const checkStatus = (response: Response) => {
-  if (response.status >= 200 && response.status < 300) {
+  if (response.ok) {
     return response;
   }
   throw new Error(response.statusText);
