2d4703083a20e21489c4ebad6ae4e764ec43042a
Linda Hanslikova <LindaH@kentico.com>
2017-07-18 17:40:07 +0200

    KA-137 - Move reducers into corresponding folders

--- onboarding-task/src/reducers/rootReducer.ts
+++ onboarding-task/src/reducers/rootReducer.ts
@@ -1,18 +1,6 @@
 import { combineReducers } from 'redux';
-import { itemsReducer } from './ListItemReducers/itemsReducers/itemsReducer';
-import { itemIdsReducer } from './ListItemReducers/itemsReducers/itemIdsReducer';
-import { fetchingFlagReducer } from './ListItemReducers/flagReducers/fetchingFlagReducer';
-import { errorReducer } from './ListItemReducers/errorReducers/errorReducer';
-import { postingFlagReducer } from './ListItemReducers/flagReducers/postingFlagReducer';
-import { errorIdsReducer } from './ListItemReducers/errorReducers/errorIdsReducer';
+import { listReducers } from './ListItemReducers/listReducers';
 
-const rootReducer = combineReducers({
-  items: itemsReducer,
-  itemIds: itemIdsReducer,
-  isFetching: fetchingFlagReducer,
-  isPosting: postingFlagReducer,
-  errors: errorReducer,
-  errorIds: errorIdsReducer,
-});
+const rootReducer = combineReducers({list: listReducers});
 
 export { rootReducer };
