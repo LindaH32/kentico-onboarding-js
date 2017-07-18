40b6b9fec3de880e4e7e8d9077dc98e66ff12ed5
Linda Hanslikova <LindaH@kentico.com>
2017-07-18 12:50:24 +0200

    KA-137 - Make status code tests working

--- onboarding-task/test/checkingMethods/statusCodeCheck.test.ts
+++ onboarding-task/test/checkingMethods/statusCodeCheck.test.ts
@@ -1,31 +1,19 @@
 import { checkStatus } from '../../src/actionCreators/checkStatus';
 
-describe('Correctly decides whether the response from the server is valid', () => {
-  const Success = [
-    {name: 'OK', code: 200 },
-    {name: 'Created', code: 201},
-  ];
+describe('Correctly decides whether the response from the server is successful', () => {
 
-  const Errors = [
-    {name: 'Bad Request', code: 400},
-    {name: 'Forbidden', code: 403},
-    {name: 'Internal Server Error ', code: 500},
-  ];
+  it('returns response when response property ok is true', () => {
+    const successfulResponse = { ok: true } as Response;
 
-  const response = (code: number) => new Response({ status: code });
-  Success.forEach(testCase => {
-    it('returns response when ' + testCase.name + ' statusCode is received', () => {
-      const actual = checkStatus(response(testCase.code));
+    const actual = checkStatus(successfulResponse);
 
-      expect(actual).toBe(true);
-    });
+    expect(actual).toBe(successfulResponse);
   });
 
-  Errors.forEach(testCase => {
-    it('throws an error when ' + testCase.name + ' statusCode is received', () => {
-      const actual = checkStatus(response(testCase.code));
+  it('throws an error when response property ok is false', () => {
+    const failedResponse = { ok: false } as Response;
 
-      expect(actual).toBe(false);
-    });
+    expect(() => checkStatus(failedResponse)).toThrowError();
   });
 });
+
