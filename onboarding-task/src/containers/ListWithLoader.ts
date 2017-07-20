1b5076c8952a736bb5a471d682bd88f4b8e4c72a
Linda Hanslikova <LindaH@kentico.com>
2017-07-20 13:55:10 +0200

    KA-133 - Make Loader component completely generic
    By passing the path to the spinner from ListWithLoader component via props.

--- onboarding-task/src/containers/ListWithLoader.ts
+++ onboarding-task/src/containers/ListWithLoader.ts
@@ -5,14 +5,16 @@ import { ILoaderCallbackProps, ILoaderDataProps } from '../components/Loader';
 import { Loader as LoaderComponent } from '../components/Loader';
 import { fetchItems } from '../actionCreators/actionCreators';
 import { List } from './List';
+import { BIG_HEAD_SPINNER } from '../constants/paths';
 
 const mapStateToProps = (state: IAppState, _ownProps: ILoaderDataProps): ILoaderDataProps => ({
   isFetching: state.list.isFetching,
   wrappedComponent: React.createElement(List),
+  spinner: BIG_HEAD_SPINNER,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): ILoaderCallbackProps => ({
-  fetchItems: () => fetchItems(dispatch),
+  doAfterMounting: () => fetchItems(dispatch),
 });
 
 const ListWithLoader: React.ComponentClass<ILoaderDataProps> = connect(
